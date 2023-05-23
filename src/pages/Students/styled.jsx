5b39c76b6b9b7496db8edd5f3e09cec1aa2e2273
wnicolass <nicolaswgl99@gmail.com>
2023-05-23 18:59:00 +0100

    feat: create sign up form and handle sign up process

--- src/pages/Students/styled.jsx
+++ src/pages/Students/styled.jsx
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const Paragraph = styled.p``;
+export const StudentContainer = styled.table`
+  margin-top: 1rem;
+  width: 100%;
+
+  tr:not(.ProfilePic) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.2rem;
+
+    border-bottom: 1px solid #cccccc9c;
+  }
+
+  tr td {
+    text-align: center;
+  }
+
+  @media screen and (max-width: 728px) {
+    tr {
+      font-size: 0.9rem;
+    }
+    tr img,
+    tr svg {
+      width: 2rem;
+    }
+
+    tr a svg {
+      width: 1rem;
+    }
+  }
+`;
+
+export const ProfilePicture = styled.td`
+  img {
+    width: 4rem;
+    border-radius: 50%;
+  }
+`;
